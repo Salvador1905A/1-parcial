624af4a8d27150b441cd0afb1cc01923ebe4e4b0
SalvadorCA <127062704+Salvador1905A@users.noreply.github.com>
2023-03-09 10:29:16 -0600

    Update config.js

--- railway-github/src/config.js
+++ railway-github/src/config.js
@@ -1,7 +1,7 @@
 export const PORT = process.env.PORT || 19990
 
-export const DB_HOST = process.env.DB_HOST || 'mysql-114461-0.cloudclusters.net'
-export const DB_USER = process.env.DB_USER || 'Salvador'
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'salvador19'
-export const DB_NAME = process.env.DB_NAME || 'user'
+export const DB_HOST = process.env.DB_HOST || 'containers-us-west-97.railway.app'
+export const DB_USER = process.env.DB_USER || 'root'
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'v6vEnPt6WH5NNK5zaMI0'
+export const DB_NAME = process.env.DB_NAME || 'users'
 export const DB_PORT = process.env.DB_PORT || 3306
