38cfe0dac14bdc5d89c8cb11b4b82924209742c1
SalvadorCA <127062704+Salvador1905A@users.noreply.github.com>
2023-03-13 16:55:28 -0600

    Update config.js

--- railway-github/src/config.js
+++ railway-github/src/config.js
@@ -1,7 +1,7 @@
 export const PORT = process.env.PORT || 6535
 
-export const DB_HOST = process.env.DB_HOST || 'containers-us-west-97.railway.app'
+export const DB_HOST = process.env.DB_HOST || 'containers-us-west-154.railway.app'
 export const DB_USER = process.env.DB_USER || 'root'
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'v6vEnPt6WH5NNK5zaMI0'
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'tmC0LXsa5QjX5JkWw5Gg'
 export const DB_NAME = process.env.DB_NAME || 'users'
 export const DB_PORT = process.env.DB_PORT || 3306
